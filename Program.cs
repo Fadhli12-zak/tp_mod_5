ae3d1d14c7460d67c725489dc7e996ffd626cf92
fadhlidzaki <fadhlidzaki120@gmail.com>
2025-03-16 13:48:55 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ public class HaloGeneric
+{
+    public static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser<string>("Fadhli");
+    }
+    public void SapaUser<T>(T nama)
+    {
+        Console.WriteLine("Halo User" + nama);
+    }
+}
